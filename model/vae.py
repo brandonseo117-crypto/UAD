41021c26deab7a8b3062b323b8f3df5100702804
Brandon Seo <brandonseo117@gmail.com>
2026-08-09 18:59:36 -0400

    varautoencoder blocks

--- model/vae.py
+++ model/vae.py
@@ -1,5 +1,5 @@
 import torch
-from monai.networks.nets import VariationalAutoEncoder
+from monai.networks.nets import VarAutoEncoder
 
 class VAEModel(torch.nn.Module):
     def __init__(self, input_shape=(1, 128, 128, 128), latent_dim=256):
@@ -7,14 +7,25 @@ class VAEModel(torch.nn.Module):
         self.input_shape = input_shape
         self.latent_dim = latent_dim
 
-        self.vae = VariationalAutoEncoder(
+        self.vae = VarAutoEncoder(
             spatial_dims=3,
             in_channels=input_shape[0],
             out_channels=input_shape[0],
             latent_size=latent_dim,
-            hidden_sizes=[32, 64, 128],
-            num_res_units=2,
+            channels=(64, 128, 256),
+            strides = (2,2),
+            num_res_units=2
         )
 
     def forward(self, x):
         return self.vae(x)
+
+    def compute_loss(self, x):
+        recon_x, mu, logvar = self.forward(x)
+        recon_loss = torch.nn.functional.mse_loss(recon_x, x, reduction='sum')
+        kld_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        return (recon_loss + kld_loss)
+
+    def anomaly_mapping(self, x):
+        recon_x, _, _ = self.forward(x)
+        return torch.abs(x - recon_x), recon_x 
