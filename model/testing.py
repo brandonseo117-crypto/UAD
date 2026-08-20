9c0c01139f13ae4cce3a7c66d07899d974fd35ff
Brandon <brandonseo117@gmail.com>
2026-08-20 15:28:04 -0400

    testing loop

--- model/testing.py
+++ model/testing.py
@@ -3,77 +3,86 @@ from vae import VAEModel, ELBOvae
 from vit import VitAE, VitDualDomainLoss
 from architecture import MambaUAD, DualDomainLoss
 import torch
-from torchmetrics.image import PeakSignalNoiseRatio
+import numpy as np
+from calflops import calculate_flops
+from sklearn.metrics import roc_auc_score, precision_recall_curve, auc
+from torch.utils.flop_counter import FlopCounterMode
+from torchmetrics.functional.image import peak_signal_noise_ratio, structural_similarity_index_measure
+import time
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-mamba_model = MambaUAD()
-mamba_state_dict = torch.load('mamba_weights.pth', weights_only=True)
-mamba_model.load_state_dict(mamba_state_dict)
-mamba_model.eval()
-
-vae_model = VAEModel().to(device)
-vae_state_dict = torch.load('vae_weights.pth', weights_only=True)
-vae_model.load_state_dict(vae_state_dict)
-vae_model.eval()
-
 vitae_model = VitAE().to(device)
 vitae_state_dict = torch.load('vit_weights.pth', weights_only=True)
 vitae_model.load_state_dict(vitae_state_dict)
-vitae_model.eval()
-
-
-calculate_psnr = PeakSignalNoiseRatio(data_range=5.0).to(device)
-
-#VAE
-vae_loss_vals = []
-psnr_vals = []
-running_loss = 0.0
-criterion = ELBOvae()
-with torch.no_grad():
-    for idx, (input_data, label, vis_date, pt_id) in enumerate(test_loader):
-        input_data = input_data.to(device)
-        output, mu, logvar = vae_model(input_data)
-        loss = criterion(input_data, output, mu, logvar)
-        running_loss += loss.item()
-        vae_loss_vals.append(loss.item())
-        psnr_vals.append(calculate_psnr(output, input_data).item())
-
-avg_loss = running_loss / len(test_loader)
-print(f"Avg: {avg_loss}:.4f")
-
-
-# Mamba
-mamba_loss_vals = []
-criterion = DualDomainLoss()
-running_loss = 0.0
-with torch.no_grad():
-    for idx, (input_data, label, vis_date, pt_id) in enumerate(test_loader):
-        input_data = input_data.to(device)
-        output, encs, decs = mamba_model(input_data)
-        loss = criterion(input_vol=output, target_vol=input_data, enc_feats=encs, dec_feats=decs)
-        running_loss += loss.item()
-        mamba_loss_vals.append(loss.item())
-        
-        psnr_vals.append(calculate_psnr(output, input_data).item())
-
-avg_loss = running_loss / len(test_loader)
-print(f"Avg: {avg_loss}:.4f")
-
-# VitAE
-
-vit_loss_vals = []
+model = vitae_model.eval()
 criterion = VitDualDomainLoss()
-running_loss = 0.0
-with torch.no_grad():
-    for idx, (input_data, label, vis_date, pt_id) in enumerate(test_loader):
-        input_data = input_data.to(device)
-        output, hidden_state = vitae_model(input_data)
-        loss = criterion(input_vol=output, target_vol=input_data, hidden_states=hidden_state)
-        running_loss += loss.item()
-        vit_loss_vals.append(loss.item())
-        psnr_vals.append(calculate_psnr(output, input_data).item())
 
-avg_loss = running_loss / len(test_loader)
-print(f"Avg: {avg_loss}:.4f")
+batch_latencies = []
+
+ad_loss = []
+ad_psnr = []
+ad_ssim = []
+
+mci_loss = []
+mci_psnr = []
+mci_ssim = []
+
+cn_loss = []
+cn_psnr = []
+cn_ssim = []
+
+
+for idx, (data, label, vis_date, pt_id) in enumerate(test_loader):
+    input_data = data.to(device)
+    torch.cuda.synchronize(device)
+    start_time = time.perf_counter()
+    output, hidden_state = model(input_data)
+    torch.cuda.synchronize(device) 
+    end_time = time.perf_counter()
+    batch_latencies.append(end_time - start_time)
+    loss = criterion(output, input_data, hidden_state)
+    psnr_val = peak_signal_noise_ratio(output, input_data, data_range=6.53).item()
+    ssim_val = structural_similarity_index_measure(output, input_data, data_range=6.53).item()
+    
+    if label == 0:
+        ad_loss.append(loss.item())
+        ad_psnr.append(psnr_val)
+        ad_ssim.append(ssim_val)
+    elif label == 1:
+        cn_loss.append(loss.item())
+        cn_psnr.append(psnr_val)
+        cn_ssim.append(ssim_val)
+    else:
+        mci_loss.append(loss.item())
+        mci_psnr.append(psnr_val)
+        mci_ssim.append(ssim_val)
+
+mean_batch_latency = sum(batch_latencies) / len(batch_latencies)
+overall_throughput = 1 / mean_batch_latency
+print(f'Mean batch latency: {mean_batch_latency} and overall throughput: {overall_throughput}')
+
+flops, macs, params = calculate_flops(model=model, input_shape=(1, 1, 128, 128, 128))
+print(f'flops: {flops}   macs: {macs}   params: {params}')
+
+def get_auc_metrics(control_values, anomaly_values, loss=True):
+    true = np.array([0] * len(control_values) + [1] * len(anomaly_values))
+    if loss==True:
+        scores = -1 * np.array(control_values + anomaly_values)
+    else:
+        scores = np.array(control_values + anomaly_values)
+    
+    roc_auc = roc_auc_score(y_true=true, y_score=scores)
+    precision, recall, _ = precision_recall_curve(y_true=true, probas_pred=scores)
+    prc_auc = auc(recall, precision)
+
+    print(f'ROC AUC (CN vs AD): {roc_auc:.4f}')
+    print(f'PRC AUC (CN vs AD): {prc_auc:.4f}')
+
+get_auc_metrics(cn_loss, ad_loss)
+get_auc_metrics(cn_loss, mci_loss)
+get_auc_metrics(cn_psnr, ad_psnr, loss=False)
+get_auc_metrics(cn_psnr, mci_psnr, loss=False)
+get_auc_metrics(cn_ssim, ad_psnr, loss=False)
+get_auc_metrics(cn_ssim, mci_ssim, loss=False)
